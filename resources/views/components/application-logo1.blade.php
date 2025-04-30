7e59ca8f14765500773f0c780616b2bceb6eb793
Henry J. <hjabunan@gmail.com>
2025-04-30 17:57:11 +0800

    0430-updatev2

--- resources/views/components/application-logo1.blade.php
+++ resources/views/components/application-logo1.blade.php
@@ -1,1 +1,2 @@
-<img class="h-auto max-w-lg rounded-lg" src="{{ URL::to('/') }}/bpa.png" alt="">
+{{-- <img class="h-auto max-w-lg rounded-lg" src="{{ URL::to('/') }}/bpa-logo.png" alt=""> --}}
+<img class="w-[100px] h-auto rounded-lg" src="{{ URL::to('/') }}/bpa-logo.png" alt="">
