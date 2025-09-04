7719ac4bd7103df485d83cf4c8fa41eb5809cfac
Henry J. <124009038+hjabunan@users.noreply.github.com>
2025-09-04 14:49:17 +0800

    Update index.blade.php
    process and checkpoint block

--- resources/views/bpa-systemconfig/sc-forms/index.blade.php
+++ resources/views/bpa-systemconfig/sc-forms/index.blade.php
@@ -218,7 +218,7 @@
                                             </div>
 
                                             <!-- Desktop Table -->
-                                            <table id="processTable" class="hidden md:table w-full text-sm text-left text-gray-500">
+                                            <table id="processTable" class="hidden md:block w-full text-sm text-left text-gray-500">
                                                 <thead class="text-xs text-gray-700 uppercase bg-white sticky top-[50px] shadow-md">
                                                     <tr>
                                                         <th class="px-6 py-2 text-center w-[10%]">Action</th>
@@ -350,7 +350,7 @@
                                         </div>
                                     </div>
                                     <!-- Desktop Table -->
-                                    <table id="cpointTable" class="hidden sm:table w-full text-sm text-left text-gray-500" style="max-height: calc(100% - 49px);">
+                                    <table id="cpointTable" class="hidden sm:block w-full text-sm text-left text-gray-500" style="max-height: calc(100% - 49px);">
                                         <thead class="text-xs text-gray-700 uppercase bg-white sticky top-[50px] shadow-md">
                                             <tr>
                                                 <th scope="col" class="px-6 py-2 text-center" style="width: 10%;">
