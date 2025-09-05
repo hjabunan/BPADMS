bcfa02414b7db3984d199f4a1fb76e9305caa504
Henry J. <124009038+hjabunan@users.noreply.github.com>
2025-09-05 09:30:40 +0800

    Update index.blade.php

--- resources/views/bpa-systemconfig/sc-forms/index.blade.php
+++ resources/views/bpa-systemconfig/sc-forms/index.blade.php
@@ -351,7 +351,7 @@
                                     </div>
                                     <!-- Desktop Table -->
                                     <!-- <div class="w-full overflow-x-auto"> -->
-                                        <table id="cpointTable" class="sm:block w-full text-sm text-left text-gray-500" style="max-height: calc(100% - 49px);">
+                                        <table id="cpointTable" class="md:block w-full text-sm text-left text-gray-500" style="max-height: calc(100% - 49px);">
                                             <thead class="text-xs text-gray-700 uppercase bg-white sticky top-[50px] shadow-md">
                                                 <tr>
                                                     <th scope="col" class="px-6 py-2 text-center" style="width: 10%;">
